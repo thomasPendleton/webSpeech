b1073606e962b646d54e1439cdc16202389a6e11
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2020-09-22 23:15:20 -0700

    script

--- script.js
+++ script.js
@@ -75,3 +75,29 @@ function createBox(item) {
 
   // todo speak event
 }
+
+// Store voices
+let voices = [];
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} : ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// Voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
